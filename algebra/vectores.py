e3054c31b44f590859b0858c5d7c5be931df17f2
ona03 <ona.yan.bonastre@estudiantat.upc.edu>
2024-03-31 19:05:25 +0200

    Tarea3

--- algebra/vectores.py
+++ algebra/vectores.py
@@ -1,7 +1,7 @@
 """
     Tercera tarea de APA - manejo de vectores
 
-    Nombre y apellidos:
+    Nombre y apellidos: Ona Bonastre Martí
 """
 
 class Vector:
@@ -10,7 +10,7 @@ class Vector:
     """
     def __init__(self, iterable):
         """
-        Costructor de la clase Vector. Su único argumento es un iterable con las componentes del vector.
+        Constructor de la clase Vector. Su único argumento es un iterable con las componentes del vector.
         """
 
         self.vector = [valor for valor in iterable]
@@ -85,3 +85,69 @@ class Vector:
 
         return -self + other
 
+    def __mul__(self, other):
+        """
+        Producto de dos elementos compsatibles
+        >>> v1 = Vector([1, 2, 3])
+        >>> v2 = Vector([4, 5, 6])
+        >>> v1 * 2
+        Vector([2, 4, 6])
+        >>> v1 * v2
+        Vector([4, 10, 18])
+        """
+
+        if isinstance(other, (int, float)):
+            resultado = [valor * other for valor in self.vector]
+            return Vector(resultado)
+        elif isinstance(other, Vector):
+            if len(self.vector) != len(other.vector):
+                raise ValueError("Los vectores deben ser de la misma longitud para el producto de Hadamard.")
+            resultado = [self.vector[i] * other.vector[i] for i in range(len(self.vector))]
+            return Vector(resultado)
+            
+    def __matmul__(self, other):
+        """
+        Devuelve el producto escalar de dos vectores 
+        >>> v1 = Vector([1, 2, 3])
+        >>> v2 = Vector([4, 5, 6])
+        >>> v1 @ v2
+        32
+        """
+
+        if isinstance(other, Vector):
+            resultado = sum(self.vector[i] * other.vector[i] for i in range(len(self.vector)))
+            return resultado
+
+    def __floordiv__(self, other):
+        """
+        Método para devolver la componente tangencial de un Vector
+        >>> v1 = Vector([2, 1, 2])
+        >>> v2 = Vector([0.5, 1, 0.5])
+        >>> v1 // v2
+        Vector([1.0, 2.0, 1.0])
+        """
+
+        if isinstance(other, Vector):
+            if len(self.vector) != len(other.vector):
+                raise ValueError("Los vectores deben ser de la misma longitud")
+            escalar_v1_v2 = self @ other
+            v2_cuadrado = other @ other
+            factor = escalar_v1_v2 / v2_cuadrado
+            paralela = [factor * elemento for elemento in other.vector]
+            return Vector(paralela)
+
+    def __mod__(self, other):
+        """
+        Método para devolver la componente normal de un Vector
+        >>> v1 = Vector([2, 1, 2])
+        >>> v2 = Vector([0.5, 1, 0.5])
+        >>> v1 % v2
+        Vector([1.0, -1.0, 1.0])
+        """
+
+        if isinstance(other, Vector):
+            if len(self.vector) != len(other.vector):
+                raise ValueError("Los vectores deben ser de la misma longitud")
+            paralela = self.__floordiv__(other)
+            perpendicular = self - paralela
+            return Vector(perpendicular)
